f758697fd7cd8d8a31498d9afa3da6f9524d4a3f
Nothize <nothize@gm4i1>
2010-05-11 17:10:51 +0800

    Fixed some deployment issues and use version v1.1.1.

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir dist
-xcopy src dist /d /y
+xcopy src dist /d /y /s
 java -jar d:\work\chrome\compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --externs externs.js --js src\ffeeder.js --js_output_file dist\ffeeder.js 
--- externs.js
+++ externs.js
@@ -3,6 +3,11 @@ $.each;
 $.get;
 $.attr;
 $.andSelf;
+$.appendTo;
+$.append;
+$.hide;
+$.show;
 chrome.extension.getBackgroundPage;
+chrome.extension.getURL;
 function $(){} 
 Default.fid;
