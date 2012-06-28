d539b4955f16e33d85e0a080e631f4faddd94f7b
Lok <loktsui@apex-pro.com>
2012-06-28 12:09:59 +0800

    Fix distortion of some method names
    chrome.* and $.css

--- externs.js
+++ externs.js
@@ -7,9 +7,11 @@ $.appendTo;
 $.append;
 $.hide;
 $.show;
+$.css;
 chrome.extension.getBackgroundPage;
 chrome.extension.getURL;
 chrome.browserAction.setBadgeText;
 chrome.browserAction.setTitle;
+chrome.windows.getLastFocused;
 function $(){} 
 function parseInt() {}
